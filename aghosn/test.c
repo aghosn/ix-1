3a514703d3c2fc29a3ac3f783ae64f56674f827b
aghosn <ghosn.adrien@gmail.com>
2016-10-14 16:03:45 +0200

    Some more playing with the VM

--- aghosn/test.c
+++ aghosn/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "test.h"
 #include <asm/vmx.h>
+#include "tools.h"
 
 
 uint32_t read_cr0() {
@@ -53,30 +54,31 @@ int main() {
 		return ret;
 
 	println("I'm in dune mode !");
-	if (!pgroot)
-		println("Page root is null.");
-	else
-		println("Page root is actually live!");
+	// if (!pgroot)
+	// 	println("Page root is null.");
+	// else
+	// 	println("Page root is actually live!");
 
 	
-	struct dune_tf *tf = NULL;
-	struct thread_arg *a = NULL;
+	// struct dune_tf *tf = NULL;
+	// struct thread_arg *a = NULL;
 
-	printf("The address of pgroot %p\n", pgroot);
-	printf("The size of pgroot %lu\n", sizeof(pgroot));
-	printf("The size of what it points to %d\n", sizeof(*pgroot));
+	// printf("The address of pgroot %p\n", pgroot);
+	// printf("The size of pgroot %lu\n", sizeof(pgroot));
+	// printf("The size of what it points to %d\n", sizeof(*pgroot));
 	
-	ptent_t* copy = memalign(PGSIZE, PGSIZE);
+	// ptent_t* copy = memalign(PGSIZE, PGSIZE);
 
-	printf("The address of copy %p\n", copy);
-	printf("The size of copy %lu\n", sizeof(copy));
-	printf("The size of what it points to %d\n", sizeof(*copy));
+	// printf("The address of copy %p\n", copy);
+	// printf("The size of copy %lu\n", sizeof(copy));
+	// printf("The size of what it points to %d\n", sizeof(*copy));
 
-	copy = memcpy(copy, pgroot, sizeof(*pgroot));
+	// copy = memcpy(copy, pgroot, PGSIZE);
 
-	//load_cr3((unsigned long) copy);
+	// load_cr3((unsigned long) copy);
 	
-	// uint32_t cr0 = 0xdeadbeef;
+	// printf("After the load.\n");
+	// // uint32_t cr0 = 0xdeadbeef;
 	// __asm__ __volatile__ (
 	// 	"mov %%cr0, %%rax\n\t"
 	// 	"mov %%eax, %0\n\t"
@@ -85,42 +87,54 @@ int main() {
 	// 	: "%rax" 
 	// 	);
 
-	uint32_t cr0 = read_cr0();
-	printf("cr0 = 0x%8.8X\n", cr0 >> 31);
-	printf("cr0.pG %d\n", IS_CR0_PG(cr0));
-	if (IS_CR0_PG(cr0) != 0)
-		printf("Has PG enabled.\n");
-	else 
-		println("Paging not enabled.\n");
+	// ptent_t* trial = deep_copy_pgroot(pgroot);
+	// printf("Managed to do it.\n");
 
-	println("Checking that cr3 is pgroot.");
-	if (read_cr3() == (uint64_t)pgroot)
-		println("Yeah that's the case");
-	else
-		println("Nope");
+	// uint32_t cr0 = read_cr0();
+	// printf("cr0 = 0x%8.8X\n", cr0 >> 31);
+	// printf("cr0.pG %d\n", IS_CR0_PG(cr0));
+	// if (IS_CR0_PG(cr0) != 0)
+	// 	printf("Has PG enabled.\n");
+	// else 
+	// 	println("Paging not enabled.\n");
 
-	if (cr0 & 0x1)
-		printf("Protection is enabled.\n");
-	else
-		printf("Protection is disabled\n.");
+	// println("Checking that cr3 is pgroot.");
+	// if (read_cr3() == (uint64_t)pgroot)
+	// 	println("Yeah that's the case");
+	// else
+	// 	println("Nope");
+
+	// if (cr0 & 0x1)
+	// 	printf("Protection is enabled.\n");
+	// else
+	// 	printf("Protection is disabled\n.");
 
 	/*printf("Before trying something bold\n.");
 	asm("xorl 0x1 %cr0" );*/
 
-	uint32_t cr4 = read_cr4();
-	if(cr4 & (1 << 5)) /*PAE*/
-		printf("PAE is enabled\n");
-	else 
-		printf("PAE disabled.\n");
-	//uint32_t test = 0x00002c02;
+	// uint32_t cr4 = read_cr4();
+	// if(cr4 & (1 << 5)) /*PAE*/
+	// 	printf("PAE is enabled\n");
+	// else 
+	// 	printf("PAE disabled.\n");
+
 	//load_cr3(copy);
 	//printf("After the load.\n");
-	printf("%lu\n", HOST_IA32_EFER);
+
 	
 	// unsigned low, high;
-	// asm volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(HOST_IA32_EFER));
+	// asm volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(guest_ia32_efer));
 	// unsigned result = low | ((unsigned long)high << 32);
 	// printf("%lu\n", result);
-	
+	// void* test = dune_page_alloc();
+	printf("The cr3 value is %p\n", read_cr3());
+	uint64_t test = 0xdeadbeef;
+	printf("The address on the stack %p for deadbeef.\n", &test);
+	has_a_mapping(pgroot, &test);
+
+	crawl_stats_t result = crawl(pgroot);
+	printf("The result is %u pml4 entries, %u pdpte entries, and %u pde entries.\n", result.pml4_entries, result.pdpte_entries, result.pde_entries);
+	printf("And the bigs %u and %u\n", result.pdpte_big, result.pde_big);
+		
 	return 0;
 }
